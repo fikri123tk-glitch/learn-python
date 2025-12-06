cb65b034e17470a2a1736fcc8f49fb38b2c62d03
whoami_fikri <fikri123tk@gmail.com>
2025-12-06 21:26:45 +0700

    day6

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ musang = random.randint(1,5)
 print(f"{Welcome_message} selamat datang  di program pyhton")
 
 nama_saya = input("siapa kamu: ")
+while nama_saya == "":
+    nama_saya = input("nama tidak boleh kosong, tolong masukkan namamu: ")
 
 print(f"halo {nama_saya}")
     
@@ -11,11 +13,15 @@ bentuk_lubang = "|_|"
 lubang_kosong = [bentuk_lubang]* 5
 lubang = lubang_kosong.copy()
 lubang[musang -1] = "|0_0|"
-print(f'''coba tebak musang ada di lobang nomor berapa?{lubang_kosong}''')
-      
-
+lubang_kosong = ' '.join(lubang_kosong)
+lubang = ' '.join(lubang)
+print(f'''coba tebak musang ada di lobang nomor berapa?
+      {lubang_kosong}''')
+    
 while True:
     pilihan_user = int(input("menurtumu di luang mana musang itu berada? (1-5): "))
+    while pilihan_user < 1 or pilihan_user >5:
+        pilihan_user = int(input("input tidak valid, tolong masukkan angka antara 1-5: "))
     confirm_user = input(f"apakah kamu yakin kalo {pilihan_user} jawabannya? (y/n):")
     if confirm_user == 'n':
         print("oke coba tebak lagi yaa")
